eb4104c8b2c0f77f87e7e56b0ca27a902aee9b60
Мирон <ghanin03@list.ru>
2022-01-29 21:26:39 +0300

    main

--- Platformer/main.cpp
+++ Platformer/main.cpp
@@ -9,20 +9,13 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1500, 1500), "SFML window");
     sf::Clock clock;
     float lastTime = 0;
-    std::vector<std::shared_ptr<pe::Shape>> drawable;
-    std::shared_ptr<pe::Rectangle> rect = std::make_shared<pe::Rectangle>(sf::Vector2<float>(500.0, 500.0),
-                                                                          sf::Vector2<float>(700.0, 800.0));
-    drawable.push_back(std::make_shared<pe::Rectangle>(sf::Vector2<float>(0.0, 0.0),
-                                                       sf::Vector2<float>(100.0, 100.0)));
-    drawable.push_back(rect);
-    drawable.push_back(std::make_shared<pe::Rectangle>(sf::Vector2<float>(800.0, 800.0),
-                                                       sf::Vector2<float>(950.0, 805.0)));
+    std::vector<std::shared_ptr<pe::Shape>> drawable; // все в этом векторе отрисовывается каждый кадр
+    float fps = 30;
+    drawable.push_back(std::make_shared<pe::RectObj>(sf::Vector2<float>(500.0, 0.0),
+                                                       sf::Vector2<float>(600.0, 100.0), window, fps));
     while (window.isOpen()) {
-        if(rect->isCollide(drawable[0])){
-            std::cout << "!!!\n";
-        }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+        /*if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
             rect->move(sf::Vector2<float>(1, 0));
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
@@ -33,7 +26,7 @@ int main() {
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
             rect->move(sf::Vector2<float>(0, 1));
-        }
+        }*/
         // Process events
         sf::Event event{};
         while (window.pollEvent(event)) {
@@ -46,13 +39,13 @@ int main() {
 
         //draw
         for(auto &i : drawable){
-            i->s_draw(window);
+            i->frame();
         }
         window.display();
 
         //fps
         sf::Time time = clock.getElapsedTime();
-        double fps = 1 / (time.asSeconds() - lastTime);
+        fps = 1 / (time.asSeconds() - lastTime);
         lastTime = time.asSeconds();
         window.setTitle(std::to_string(fps));
     }
